b707d5a2367432334c7ffe4adb363d992e247d12
manolenso <rlaurent@tvmag.com>
2015-10-15 07:42:05 +0200

    some code

--- index.js
+++ index.js
@@ -1,76 +1,15 @@
+  'use strict';
 var clipboard = require(clipboard);
 var cherio = require('cherio');
-
-
+var parse = require('url-parse')
 module.exports = {
     // Extend website resources and html
-    website: {
+    book: {
         assets: "./assets",
         js: [
             "embed-clipboard.js"
-        ],
-        css: [
-            "test.css"
-        ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
-
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
+        ]
     },
-
-    // Extend ebook resources and html
-    website: {
-        assets: "./book",
-        js: [
-            "test.js"
-        ],
-        css: [
-            "test.css"
-        ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
-
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
-    },
-
-    // Extend templating blocks
-    blocks: {
-        // Author will be able to write "{% myTag %}World{% endMyTag %}"
-        myTag: {
-            process: function(blk) {
-                return "Hello "+blk.body;
-            }
-        }
-    },
-
-    // Extend templating filters
-    filters: {
-        // Author will be able to write "{{ 'test'|myFilter }}"
-        myFilter: function(s) {
-            return "Hello "+s;
-        }
-    },
-
     // Hook process during build
     hooks: {
       // This is called before the book is generated
@@ -79,7 +18,7 @@ module.exports = {
       },
       page: function(page) {
         var config = _.defaults(this.options.pluginsConfig
-          .codepen, defaultConfig);
+          .clipboard, defaultConfig);
         _.each(page.sections, function(section) {
           var $ = cheerio.load(section.content);
           $('a').filter(function() {
